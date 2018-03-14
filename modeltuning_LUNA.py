3b4717b56c33aa4c371a4c49d8d6abee435136fc
Ubuntu <ubuntu@ip-10-0-0-9.ap-southeast-1.compute.internal>
2018-03-14 04:57:45 +0000

    sample run LUNA 1.data prep 2. Model Tuning successfully

--- modeltuning_LUNA.py
+++ modeltuning_LUNA.py
@@ -26,6 +26,11 @@ def dice_coef(y_true, y_pred):
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
+def dice_coef_np(y_true, y_pred):
+    y_true_f = y_true.flatten()
+    y_pred_f = y_pred.flatten()
+    intersection = np.sum(y_true_f * y_pred_f)
+    return (2. * intersection + SMOOTH) / (np.sum(y_true_f) + np.sum(y_pred_f) + SMOOTH)
 
 def get_unet(img_size=(512, 512)):
     #rows, cols = img_size
--- utils_LUNA.py
+++ utils_LUNA.py
@@ -6,6 +6,8 @@ import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
+import bcolz
+
 
 def get_filename(case, file_list):
     '''
@@ -14,4 +16,12 @@ def get_filename(case, file_list):
     for f in file_list:
         if case in f:
             return(f)
-        
+    
+    
+def save_array(fname, arr):
+    c = bcolz.carray(arr, rootdir=fname, mode='w')
+    c.flush()
+    
+    
+def load_array(fname):
+    return np.array(bcolz.open(rootdir=fname))
